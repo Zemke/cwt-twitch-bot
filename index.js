6cd681c983740573a78df284c451c1a9795ff8a5
Zemke <florian@zemke.io>
2020-11-16 20:18:08 +0100

    Externalize listener

--- index.js
+++ index.js
@@ -32,61 +32,11 @@ async function onMessageHandler(target, context, msg, self) {
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
   if (process.env.LISTEN === '1') {
-    listenToMessages();
+    request.listenToMessages(client.getChannels, (channel, message) => {
+      client.say(channel, message);
+    });
   } else {
     console.info("Set env LISTEN to 1 to listen for CWT chat messages.");
   }
 }
-
-function listenToMessages() {
-  const posted = {};
-  const url = 'https://cwtsite.com/api/message/listen';
-  console.info("Listening to messages", url);
-  const es = new EventSource(url);
-  es.onmessage = e => console.info("es message", e);
-  es.onerror = e => console.error("es error", e);
-  const starting = Date.now();
-  es.addEventListener("EVENT", e => {
-    console.info("EVENT", e);
-    const message = JSON.parse(e.data);
-    if (Date.now() - starting <= 3000) {
-      client.getChannels().forEach(channel => {
-        if (!(channel in posted)) posted[channel] = [];
-        posted[channel].push(message.id);
-      });
-      console.info('Discarding as probable part of initial batch');
-      return;
-    }
-    client.getChannels().forEach(channel => {
-      if (!(channel in posted)) posted[channel] = [];
-      if (!posted[channel].includes(message.id)) {
-        console.info(`Scheduling for sending message to ${channel}`);
-        client.say(channel, format(message));
-        posted[channel].push(message.id);
-      } else {
-        console.info(`${channel} already received message.`);
-      }
-    });
-  });
-
-  function format({category, author, body, newsType}) {
-    if (category === 'SHOUTBOX') {
-      return `${author.username} via CWT: “${body}”`;
-    }
-    let res = author.username + " ";
-    if (newsType === "REPORT" || newsType === "COMMENT") {
-      const [gameid, home, away, scoreh, scorea] = body.split(',');
-      res += `${newsType.toLowerCase()}ed ${home} ${scoreh}–${scorea} ${away} \
-(https://cwtsite.com/games/${gameid})`.trim();
-    } else if (newsType === "RATING") {
-      const [gameid, home, away, scoreh, scorea, rating] = body.split(',');
-      res += `${rating}d ${home} ${scoreh}–${scorea} ${away} (https://cwtsite.com/games/${gameid})`;
-    } else if (newsType === "STREAM" || newsType === "TWITCH_MESSAGE") {
-      console.info("Ignoring newsType", newsType);
-    } else {
-      console.warn("Unhandled newsType:", newsType);
-    }
-    return res;
-  }
-};
 
--- request.js
+++ request.js
@@ -37,12 +37,67 @@ function request(method, path, data) {
   });
 }
 
+function listenToMessages(channels, cb) {
+  const posted = {};
+  const url = 'https://cwtsite.com/api/message/listen';
+  console.info("Listening to messages", url);
+  const es = new EventSource(url);
+  es.onmessage = e => console.info("es message", e);
+  es.onerror = e => console.error("es error", e);
+  const starting = Date.now();
+  es.addEventListener("EVENT", e => {
+    console.info("EVENT", e);
+    const message = JSON.parse(e.data);
+    if (Date.now() - starting <= 3000) {
+      channels.forEach(channel => {
+        if (!(channel in posted)) posted[channel] = [];
+        posted[channel].push(message.id);
+      });
+      console.info('Discarding as probable part of initial batch');
+      return;
+    }
+    channels.forEach(channel => {
+      if (!(channel in posted)) posted[channel] = [];
+      if (!posted[channel].includes(message.id)) {
+        console.info(`Scheduling for sending message to ${channel}`);
+        cb(channel, format(message));
+        posted[channel].push(message.id);
+      } else {
+        console.info(`${channel} already received message.`);
+      }
+    });
+  });
+
+  function format({category, author, body, newsType}) {
+    if (category === 'SHOUTBOX') {
+      return `${author.username} via CWT: “${body}”`;
+    }
+    let res = author.username + " ";
+    if (newsType === "REPORT" || newsType === "COMMENT") {
+      const [gameid, home, away, scoreh, scorea] = body.split(',');
+      res += `${newsType.toLowerCase()}ed ${home} ${scoreh}–${scorea} ${away} \
+(https://cwtsite.com/games/${gameid})`.trim();
+    } else if (newsType === "RATING") {
+      const [gameid, home, away, scoreh, scorea, rating] = body.split(',');
+      res += `${rating}d ${home} ${scoreh}–${scorea} ${away} (https://cwtsite.com/games/${gameid})`;
+    } else if (newsType === "STREAM" || newsType === "TWITCH_MESSAGE") {
+      console.info("Ignoring newsType", newsType);
+    } else {
+      console.warn("Unhandled newsType:", newsType);
+    }
+    return res;
+  }
+};
+
 const api = {
   post(path, data) {
     return request('POST', path, data);
   },
   get(path) {
     return request('GET', path);
+  },
+  listen(channels, cb) {
+    return channels(channels, cb);
   }
 };
 
