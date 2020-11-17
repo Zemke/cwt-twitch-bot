278cefa35bcf120a1ff5d43d72d28419848e11db
Zemke <florian@zemke.io>
2020-11-17 01:43:12 +0100

    Whitespace

--- handle.js
+++ handle.js
@@ -28,7 +28,6 @@ async function handleMessage(msg, username) {
     return (`Thanks for asking, ${name}, the best site in the wormy world is of course cwtsite.com`);
   } else if (command.startsWith('!cwtchat')) {
     try {
-
       if (!message) {
         return ("Enter your message after the command that is then sent to the cwtsite.com chat.");
       }
